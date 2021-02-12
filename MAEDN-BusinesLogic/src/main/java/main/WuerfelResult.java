4763ea011b548e8fae492a8dd5d055ad6105a128
ChristianMarzianGithub <cmarzian91@gmail.com>
2021-02-12 03:48:39 -0800

    newly added

---
+++ MAEDN-BusinesLogic/src/main/java/main/WuerfelResult.java
@@ -0,0 +1,14 @@
+package main;
+
+public class WuerfelResult {
+
+	private int result = 0;
+
+	public int getResult() {
+		return result;
+	}
+
+	public void setResult(int result) {
+		this.result = result;
+	}
+}
